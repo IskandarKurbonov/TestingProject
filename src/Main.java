fb9bda1535e6d0c7dcff8a821057d5d040c7c065
IskandarKurbonov <kurbonoviskandar23@gmail.com>
2022-10-23 23:49:33 +0300

    Commit v1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This code start in new life!");
+        System.out.println("This is the second commit!");
     }
 }
