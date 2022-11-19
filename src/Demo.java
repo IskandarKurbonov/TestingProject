bec69cb3cb1ac4d540373bb84b2eb4d0f1eb2c06
IskandarKurbonov <kurbonoviskandar23@gmail.com>
2022-11-19 05:18:51 +0300

    Commit v1.2

---
+++ src/Demo.java
@@ -0,0 +1,39 @@
+// Статический импорт библиотеки
+import static java.lang.Math.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        // Скорость свободного падения
+        final double g = 9.8;
+        // Угол к горизонту в градусах
+        double alpha = 30;
+        // Масса тела (в кг)
+        double m = 0.1;
+        // Коэффициент сопротивления воздуха (Н*с/м)
+        double gamma = 0.1;
+        // Скорость тела (м/с)
+        double V = 100.0;
+        // Время
+        double t = 1.0;
+        // Координаты тела
+        double x,y;
+        // Перевод градусов в радианы
+        alpha/=180/PI;
+        // Вычисление координат:
+        x = V*m*cos(alpha)/gamma*(1-exp(-gamma*t/m));
+        y = m*(V*sin(alpha)*gamma+m*g)/gamma/gamma*
+                (1-exp(-gamma*t/m))-m*g*t/gamma;
+        // Вывод информации
+        System.out.println(
+                "Координаты тела для t="+t+" сек:\n x="+x+
+                        "м\ny=" +y+ "м"
+        );
+        System.out.println("Параметры:");
+        System.out.println("Угол alpha=" +alpha/PI*180+" градусов"
+        );
+        System.out.println("Скорость V=" +V+ " м/с");
+        System.out.println("Коэффициент сопротивления gamma=" +gamma+ "Н*с/м");
+        System.out.println("Масса тела " +m+ " кг");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.Adler32;
+import java.util.zip.CRC32;
+
+/**
+ * Пример использования классов Adler32 и CRC32
+ * */
 public class Main {
+    private static File file;
+
+    /**
+     * Функция для проверки аргументов командной строки
+     * @param args аргументы командной строки
+     * @return true, если мы корректно передали неообходимые аргументы, и false в противном случае
+     * */
+    private static boolean checkArgs(String[] args) {
+        /*Проверяем что программе передан путь к файлу*/
+        if (args.length == 0) {
+            System.out.println("Введите путь до файла: ");
+            return false;
+        }
+
+        String fileName = args[0];
+        file = new File(fileName);
+
+        /*Проверяем наличие файла на диске*/
+        if (!file.exists()) {
+            System.out.println("Данного файла не существует");
+            return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("This code start in new life!");
-        System.out.println("This is the second commit!");
+        /*Проверяем аргументы командной строки*/
+        if (checkArgs(args)) {
+            /*Создаем объект класса CRC32*/
+            CRC32 crc32 = new CRC32();
+            /*Создаем объект класса Adler32*/
+            Adler32 adler32 = new Adler32();
+
+            byte[] buffer = new byte[1024];
+            int countBytes = 0;
+
+            try (FileInputStream in = new FileInputStream(file)) {
+                /*читаем байты из файла*/
+                while ((countBytes = in.read(buffer)) != -1) {
+                    /*обнавляем контрольную сумму у объектов*/
+                    crc32.update(buffer, 0, countBytes);
+                    adler32.update(buffer, 0, countBytes);
+                }
+            } catch (IOException ignore) {}
+
+            /*Выводим контрольные суммы на экран*/
+            System.out.println("CheckSum: " + crc32.getValue() + "(CRC32)");
+            System.out.println("CheckSum: " + adler32.getValue() + "(Adler32)");
+        }
     }
 }
